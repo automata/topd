eb932c948cb655916912302088c7e914bc93de2e
Vilson Vieira <vilson@void.cc>
2010-04-16 16:59:10 -0300

    bug fix at edit. complete for using with the basic pd boxes

--- topd.py
+++ topd.py
@@ -1,62 +1,79 @@
 import socket
 
 class Box(object):
-    def __init__(self, patch, fudi_msg, x, y):
+    def __init__(self, patch, fudi_token, label, x, y):
         self.x = x
         self.y = y
         self.patch = patch
-        self.fudi_msg = fudi_msg
+        self.fudi_token = fudi_token
+        self.label = label
         self.patch.boxes.append(self)
         self.patch.editmode(1)
-        self.patch.s.send('pd-%s %s;' % (patch.filename, fudi_msg))
+        self.patch.send('%s %i %i %s' % (fudi_token, x, y, label))
 
     def delete(self):
-        self.patch.editmode(1)
-        self.patch.s.send('pd-%s mouse %i %i 1 0;' %
-                          (self.patch.filename, self.x - 1, self.y - 1))
-        self.patch.s.send('pd-%s motion %i %i 0;' %
-                          (self.patch.filename, self.x + 1, self.y + 1))
-        self.patch.s.send('pd-%s mouseup %i %i 1 0;' %
-                          (self.patch.filename, self.x + 1, self.y + 1))
-        self.patch.s.send('pd-%s cut;' % self.patch.filename)
+        #self.patch.editmode(1)
+        self.patch.send('mouse %i %i 1 0' % (self.x - 1, self.y - 1))
+        self.patch.send('motion %i %i 0' % (self.x + 1, self.y + 1))
+        self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
+        self.patch.send('cut')
         self.patch.boxes.remove(self)
 
+    def __str__(self):
+        return 'Hi... I am a box :-P'
+
     def move(self, x, y):
-        self.patch.editmode(1)
-        self.patch.s.send('pd-%s mouse %i %i 1 0;' %
-                          (self.patch.filename, self.x + 1, self.y + 1))
-        self.patch.s.send('pd-%s motion %i %i 0;' %
-                          (self.patch.filename, x, y))
-        self.patch.s.send('pd-%s mouseup 10 10 0 1;' % self.patch.filename)
+        #self.patch.editmode(1)
+        self.patch.send('mouse %i %i 1 0' % (self.x + 1, self.y + 1))
+        self.patch.send('motion %i %i 0' % (x, y))
+        self.patch.send('mouseup %i %i 1 0' % (x, y))
+        self.patch.send('mouse %i %i 1 0' % (self.x - 2, self.y - 2))
+        self.patch.send('mouseup %i %i 1 0' % (self.x -2, self.y - 2))
         self.x = x
         self.y = y
         self.patch.boxes[self.patch.boxes.index(self)] = self
 
     def click(self):
-        self.patch.editmode(0)
-        self.patch.s.send('pd-%s mouse %i %i 1 0;' %
-                          (self.patch.filename, self.x + 1, self.y + 1))
-        self.patch.s.send('pd-%s mouseup %i %i 0 1;' %
-                          (self.patch.filename, self.x + 1, self.y + 1))
+        #self.patch.editmode(0)
+        self.patch.send('mouse %i %i 1 0' % (self.x + 1, self.y + 1))
+        self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
 
-    def edit(self, newbox):
-        print 'Sorry... not implemented yet :-P'
+    def edit(self, label):
+        self.unselect()
+        self.click()
+
+        for c in label:
+            self.patch.send('key 1 %i 0' % ord(c))
+            self.patch.send('key 0 %i 0' % ord(c))
+
+        # precisa atualizar a lista de boxes?! e as connections?!
+        self.unselect()
+        self.label = label
+
+    def select(self):
+        #self.patch.editmode(1)
+        self.patch.send('mouse %i %i 1 0' % (self.x - 2, self.y - 2))
+        self.patch.send('motion %i %i 0' % (self.x + 1, self.y + 1))
+        self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
+
+    def unselect(self):
+        #self.patch.editmode(1)
+        self.patch.send('mouse %i %i 1 0' % (self.x - 1, self.y - 1))
+        self.patch.send('mouseup %i %i 1 0' % (self.x - 1, self.y - 1))
 
     def connect(self, source_outlet, target_box, target_inlet):
-        self.patch.editmode(1)
-        self.patch.s.send('pd-%s connect %i %i %i %i;' %
-                          (self.patch.filename,
-                           self.patch.boxes.index(self) + 1, source_outlet,
-                           self.patch.boxes.index(target_box) + 1, target_inlet))
+        #self.patch.editmode(1)
+        self.patch.send('connect %i %i %i %i' %
+                        (self.patch.boxes.index(self) + 1, source_outlet,
+                         self.patch.boxes.index(target_box) + 1, target_inlet))
         c = Connection(self, source_outlet, target_box, target_inlet)
         self.patch.connections.append(c)
 
     def disconnect(self, source_outlet, target_box, target_inlet):
-        self.patch.editmode(1)
-        self.patch.s.send('pd-%s disconnect %i %i %i %i;' %
-                          (self.patch.filename,
-                           self.patch.boxes.index(self) + 1, source_outlet,
-                           self.patch.boxes.index(target_box) + 1, target_inlet))
+        #self.patch.editmode(1)
+        self.patch.send('disconnect %i %i %i %i' %
+                        (self.patch.boxes.index(self) + 1, source_outlet,
+                         self.patch.boxes.index(target_box) + 1, target_inlet))
         for c in self.patch.connections:
             if ((c.source_box == self) and
                 (c.source_outlet == source_outlet) and
@@ -65,41 +82,43 @@ class Box(object):
                 break
         self.patch.connections.remove(c)
 
-
 class Object(Box):
-    def __init__(self, patch, name, arguments, x, y):
-        super(Object, self).__init__(patch,
-                                     'obj %i %i %s %s' %
-                                     (x, y, name, arguments),
-                                     x, y)
-        self.name = name
-        self.arguments = arguments
+    def __init__(self, patch, label, x, y):
+        super(Object, self).__init__(patch, 'obj', label, x, y)
+        splitted_label = label.split(' ')
+        self.name = splitted_label[0]
+        self.arguments = splitted_label[1:]
+
+    def __str__(self):
+        return '[ %s ]' % self.label
 
 class Message(Box):
-    def __init__(self, patch, value, x, y):
-        super(Message, self).__init__(patch,
-                                      'msg %i %i %s' % (x, y, value),
-                                      x, y)
-        self.value = value
+    def __init__(self, patch, label, x, y):
+        super(Message, self).__init__(patch, 'msg', label, x, y)
+
+    def __str__(self):
+        return '[ %s (' % self.label
 
 class Number(Box):
     def __init__(self, patch, x, y):
-        super(Number, self).__init__(patch,
-                                     'floatatom %i %i' % (x, y),
-                                     x, y)
+        super(Number, self).__init__(patch, 'floatatom', x, y)
+
+    def __str__(self):
+        return 'Hi... I am a number ;-)'
 
 class Symbol(Box):
     def __init__(self, patch, x, y):
-        super(Symbol, self).__init__(patch,
-                                     'symbolatom %i %i' % (x, y),
-                                     x, y)
+        super(Symbol, self).__init__(patch, 'symbolatom', x, y)
+
+    def __str__(self):
+        return 'Hi... I am a symbol ;-)'
 
 class Comment(Box):
-    def __init__(self, patch, value, x, y):
-        super(Comment, self).__init__(patch,
-                                      'text %i %i %s' % (x, y, value),
-                                      x, y)
-        self.value = value
+    def __init__(self, patch, label, x, y):
+        super(Comment, self).__init__(patch, 'text', label, x, y)
+
+    def __str__(self):
+        return '" %s "' % self.label
 
 class GUI(Box):
     pass
@@ -111,6 +130,9 @@ class Connection(object):
         self.target_box = target_box
         self.target_inlet = target_inlet
 
+    def __str__(self):
+        return '%s %i->%i %s' % (self.source_box.__str__(), self.source_outlet, self.target_inlet, self.target_box.__str__())
+
 class Patch(object):
     def __init__(self, filename='recebe.pd', hostname='localhost', port=3006):
         self.filename = filename
@@ -121,59 +143,60 @@ class Patch(object):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((hostname, port))
 
-    def create(self, box):
-        self.boxes.append(box)
-        self.editmode(1)
-        self.s.send('pd-%s %s;' % (self.filename, box.fudi_msg))
-
-    def delete(self, box):
-        self.editmode(1)
-        self.s.send('pd-%s mouse %i %i 1 0;' %
-                    (self.filename, box.x - 1, box.y - 1))
-        self.s.send('pd-%s motion %i %i 0;' %
-                    (self.filename, box.x + 1, box.y + 1))
-        self.s.send('pd-%s mouseup %i %i 1 0;' %
-                    (self.filename, box.x + 1, box.y + 1))
-        self.s.send('pd-%s cut;' % self.filename)
-        self.boxes.remove(box)
-
-    def move(self, box, x, y):
-        self.editmode(1)
-        self.s.send('pd-%s mouse %i %i 1 0;' %
-                    (self.filename, box.x + 1, box.y + 1))
-        self.s.send('pd-%s motion %i %i 0;' %
-                    (self.filename, x, y))
-        self.s.send('pd-%s mouseup 0 0 1 0;' % self.filename)
-        box.x = x
-        box.y = y
-        self.boxes[self.boxes.index(box)] = box
-
-    def click(self, box):
-        self.editmode(0)
-        self.s.send('pd-%s mouse %i %i 1 0;' %
-                    (self.filename, box.x + 1, box.y + 1))
-        self.s.send('pd-%s mouseup %i %i 0 1;' %
-                    (self.filename, box.x + 1, box.y + 1))
-
-    def edit(self, box, newbox):
-        print 'Sorry... not implemented yet :-P'
-
-    def connect(self, source_box, source_outlet, target_box, target_inlet):
-        self.editmode(1)
-        self.s.send('pd-%s connect %i %i %i %i;' %
-                    (self.filename,
-                     self.boxes.index(source_box) + 1, source_outlet,
-                     self.boxes.index(target_box) + 1, target_inlet))
-
-    def disconnect(self, source_box, source_outlet, target_box, target_inlet):
-        self.editmode(1)
-        self.s.send('pd-%s disconnect %i %i %i %i;' %
-                    (self.filename,
-                     self.boxes.index(source_box) + 1, source_outlet,
-                     self.boxes.index(target_box) + 1, target_inlet))
+    def __str__(self):
+        str = 'Remote patch %s connected at %s:%i.\n\n' % (self.filename, self.hostname, self.port)
+        if self.boxes != []:
+            str += '%i boxes:\n' % len(self.boxes)
+            for b in self.boxes:
+                str += '\t%s\n' % b.__str__()
+        else:
+            str += 'No boxes yet.\n'
+        if self.connections != []:
+            str += '%i connections:\n' % len(self.connections)
+            for c in self.connections:
+                str += '\t%s\n' % c.__str__()
+        else:
+            str += 'No connections yet.\n'
+        return str
 
     def editmode(self, state):
-        self.s.send('pd-%s editmode %i;' % (self.filename, state))
+        self.send('editmode %i' % state)
         
     def dsp(self, state):
         self.s.send('pd dsp %i;' % state)
+
+    def send(self, fudi_msg):
+        self.s.send('pd-%s %s;' % (self.filename, fudi_msg))
+
+    def clear(self):
+        for b in self.boxes:
+            b.delete()
+
+        for c in self.connections:
+            c.delete()
+            
+    # DEPRECATED: manter os metodos abaixo para garantir compatibilidade?!
+
+    def delete(self, box):
+        box.delete()
+
+    def move(self, box, x, y):
+        box.move(x, y)
+
+    def click(self, box):
+        box.click()
+
+    def edit(self, box, label):
+        box.edit(label)
+
+    def select(self, box):
+        box.select()
+
+    def unselect(self, box):
+        box.unselect()
+
+    def connect(self, source_box, source_outlet, target_box, target_inlet):
+        source_box.connect(source_outlet, target_box, target_inlet)
+
+    def disconnect(self, source_box, source_outlet, target_box, target_inlet):
+        source_box.disconnect(source_outlet, target_box, target_inlet)
