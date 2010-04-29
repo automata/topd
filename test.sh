9e8c3ec895f60ab32e79a551d0a9275ceb2b7318
Vilson Vieira <vilson@void.cc>
2010-04-29 20:51:25 -0300

    started documentation (pydoc). organized the files/directories.

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pd receive.pd &
-sleep 2
-python test.py
--- topd.py
+++ topd.py
@@ -1,9 +1,34 @@
+"""
+->pd - A Simple (Python|Scheme) Interface to Pure Data
+"""
+
+__version__ = '0.1'
+__author__ = 'Vilson Vieira <http://automata.cc>'
+__license__ = 'GNU Public License'
+
 import socket
 
 # boxes ########################################################################
 
 class Box(object):
+    """
+    Mother of all PD elements (boxes). All the main operations are done here
+    sending FUDI messages to PD.
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    fudi_token -- the FUDI type (obj, msg, nmb, ...)
+    label -- the label of the box (osc~ 440, dac~, ...)
+    x -- box's x coordinate in the patch
+    y -- box's y coordinate in the patch
+    """
     def __init__(self, patch, fudi_token, label, x, y):
+        """
+        Builds a box, initializing arguments and sending a FUDI message do PD,
+        creating the box.
+
+        Adds the box to Patch's boxes list too.
+        """
         if x is None and y is None:
             freex, freey = patch.next_freeposition()
             self.x = freex
@@ -19,6 +44,13 @@ class Box(object):
         self.patch.send('%s %i %i %s' % (fudi_token, self.x, self.y, label))
 
     def delete(self):
+        """
+        Removes the box from the patch.
+
+        Example:
+        osc = Object(p, 'osc~ 440', 100, 100)
+        osc.delete()
+        """
         self.patch.send('mouse %i %i 1 0' % (self.x - 1, self.y - 1))
         self.patch.send('motion %i %i 0' % (self.x + 1, self.y + 1))
         self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
@@ -26,9 +58,19 @@ class Box(object):
         self.patch.boxes.remove(self)
 
     def __str__(self):
+        """
+        Shows the box in a pretty formated ASCII-art way :-).
+        """
         return 'Hi... I am a box :-P'
 
     def move(self, x, y):
+        """
+        Moves the box to the new position at coordinates x and y.
+
+        Example:
+        osc = Object(p, 'osc~ 440', 100, 100)
+        osc.move(150, 200)
+        """
         self.patch.send('mouse %i %i 1 0' % (self.x + 1, self.y + 1))
         self.patch.send('motion %i %i 0' % (x, y))
         self.patch.send('mouseup %i %i 1 0' % (x, y))
@@ -39,10 +81,24 @@ class Box(object):
         self.patch.boxes[self.patch.boxes.index(self)] = self
 
     def click(self):
+        """
+        Does a mouse left-click on the box.
+
+        Example:
+        msg = Message(p, 'bang', 100, 100)
+        msg.click()
+        """
         self.patch.send('mouse %i %i 1 0' % (self.x + 1, self.y + 1))
         self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
 
     def edit(self, label):
+        """
+        Edits the label of the box, updating the old with the new one.
+
+        Example:
+        msg = Message(p, 'foo', 100, 100)
+        msg.edit('bar')
+        """
         self.unselect()
         self.click()
 
@@ -58,15 +114,41 @@ class Box(object):
         self.label = label
 
     def select(self):
+        """
+        Selects the box.
+
+        Example:
+        osc = Object(p, 'dac~', 100, 100)
+        osc.select()
+        """
         self.patch.send('mouse %i %i 1 0' % (self.x - 2, self.y - 2))
         self.patch.send('motion %i %i 0' % (self.x + 1, self.y + 1))
         self.patch.send('mouseup %i %i 1 0' % (self.x + 1, self.y + 1))
 
     def unselect(self):
+        """
+        Unselects the box.
+
+        Example:
+        osc = Object(p, 'dac~', 100, 100)
+        osc.unselect()        
+        """
         self.patch.send('mouse %i %i 1 0' % (self.x - 1, self.y - 1))
         self.patch.send('mouseup %i %i 1 0' % (self.x - 1, self.y - 1))
 
     def connect(self, source_outlet, target_box, target_inlet):
+        """
+        Connects the box with another box. You need to specify the outlet of the
+        source box and the inlet of the target box.
+
+        Adds the connection to Patch's connections list too.
+
+        Example:
+        osc = Object(p, 'osc~ 220', 100, 100)
+        dac = Object(p, 'dac~', 100, 150)
+        osc.connect(0, dac, 0) # connects outlet 0 of osc to inlet 0 of dac 
+        osc.connect(0, dac, 1) # connects outlet 0 of osc to inlet 1 of dac 
+        """
         self.patch.send('connect %i %i %i %i' %
                         (self.patch.boxes.index(self) + 1, source_outlet,
                          self.patch.boxes.index(target_box) + 1, target_inlet))
@@ -74,6 +156,19 @@ class Box(object):
         self.patch.connections.append(c)
 
     def disconnect(self, source_outlet, target_box, target_inlet):
+        """
+        Disconnects the box from another box. You need to specify the outlet
+        of the source box and the inlet of the target box.
+
+        Removes the connection from Patch's connections list too.
+        osc.select()        
+
+        Example:
+        osc = Object(p, 'osc~ 220', 100, 100)
+        dac = Object(p, 'dac~', 100, 150)
+        osc.connect(0, dac, 0) # connects outlet 0 of osc to inlet 0 of dac 
+        osc.disconnect(0, dac, 0) # disconnects the connection created above
+        """
         self.patch.send('disconnect %i %i %i %i' %
                         (self.patch.boxes.index(self) + 1, source_outlet,
                          self.patch.boxes.index(target_box) + 1, target_inlet))
@@ -86,31 +181,128 @@ class Box(object):
         self.patch.connections.remove(c)
 
 class Object(Box):
+    """
+    Defines the obj (objects) elements of PD (osc~, dac~, phasor~, ...).
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    label -- the label of the box (osc~ 440, dac~, ...)
+    x -- box's x coordinate in the patch (optional)
+    y -- box's y coordinate in the patch (optional)   
+    """
     def __init__(self, patch, label, x=None, y=None):
+        """
+        Creates a obj box with label as content at x and y position.
+
+        The x and y coordinates are optional. If you don't give one, a
+        free space in the patch will be used.
+
+        Example:
+        p = Patch()
+        osc = Object(p, 'osc~ 440', 100, 100)
+        phasor = Object(p, 'phasor~ 220')
+        """
         super(Object, self).__init__(patch, 'obj', label, x, y)
         splitted_label = label.split(' ')
         self.name = splitted_label[0]
         self.arguments = splitted_label[1:]
 
     def __str__(self):
+        """
+        Shows the object in a pretty formated ASCII-art way :-).
+
+        Example:
+        osc = Object(p, 'osc~ 440')
+        print osc
+        [ osc~ 440 ]
+        """
         return '[ %s ]' % self.label
 
 class Message(Box):
+    """
+    Defines the msg (messages) elements of PD.
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    label -- the label of the box (osc~ 440, dac~, ...)
+    x -- box's x coordinate in the patch (optional)
+    y -- box's y coordinate in the patch (optional)   
+    """    
+
     def __init__(self, patch, label, x=None, y=None):
+        """
+        Creates a msg box with label as content at x and y position.
+
+        The x and y coordinates are optional. If you don't give one, a
+        free space in the patch will be used.
+
+        Example:
+        p = Patch()
+        bang = Message(p, 'bang', 100, 100)
+        foo = Object(p, 'foo bar baz')
+        """
         super(Message, self).__init__(patch, 'msg', label, x, y)
 
     def __str__(self):
+        """
+        Shows the message in a pretty formated ASCII-art way :-).
+
+        Example:
+        bang = Message(p, 'bang')
+        print bang
+        [ bang (
+        """
         return '[ %s (' % self.label
 
 class Number(Box):
+    """
+    Defines the floatatom (numbers) elements of PD.
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    value -- the current number value
+    x -- box's x coordinate in the patch (optional)
+    y -- box's y coordinate in the patch (optional)   
+    """
+    
     def __init__(self, patch, x=None, y=None):
+        """
+        Creates a number box at x and y position.
+
+        The x and y coordinates are optional. If you don't give one, a
+        free space in the patch will be used.
+
+        Example:
+        p = Patch()
+        nmb = Number(p, 100, 100) 
+        n = Number(p)
+        """        
         super(Number, self).__init__(patch, 'floatatom', '', x, y)
         self.value = 0
 
     def __str__(self):
+        """
+        Shows the number in a pretty formated ASCII-art way :-).
+
+        Example:
+        nmb = Number(p)
+        print nmb
+        [ 0 `|
+        """        
         return '[%i `|' % self.value
 
     def increment(self, step=1):
+        """
+        Increments by a step the value of the number element.
+
+        Example:
+        nmb = Number(p)
+        print nmb
+        [ 0 `|
+        nmb.increment()
+        print nmb
+        [ 1 `|
+        """
         self.value += step
 
         self.patch.editmode(False)
@@ -123,6 +315,17 @@ class Number(Box):
         self.patch.editmode(True)
         
     def decrement(self, step=1):
+        """
+        Decrements by a step the value of the number element.
+
+        Example:
+        nmb = Number(p)
+        print nmb
+        [ 0 `|
+        nmb.decrement()
+        print nmb
+        [ -1 `|
+        """        
         self.value -= step
 
         self.patch.editmode(False)
@@ -135,17 +338,69 @@ class Number(Box):
         self.patch.editmode(True)
 
 class Symbol(Box):
+    """
+    Defines the symbolatom (symbol) elements of PD.
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    x -- box's x coordinate in the patch (optional)
+    y -- box's y coordinate in the patch (optional)   
+    """
+    
     def __init__(self, patch, x=None, y=None):
+        """
+        Creates a symbol box at x and y position.
+
+        The x and y coordinates are optional. If you don't give one, a
+        free space in the patch will be used.
+
+        Example:
+        p = Patch()
+        symbol = Symbol(p, 100, 100) 
+        s = Symbol(p)
+        """                
         super(Symbol, self).__init__(patch, 'symbolatom', '', x, y)
 
     def __str__(self):
+        """
+        Shows the symbol in a pretty formated ASCII-art way :-).
+        """                
         return 'Hi... I am a symbol ;-)'
 
 class Comment(Box):
+    """
+    Defines the text (comment) elements of PD.
+
+    Attributes:
+    patch -- the 'virtual' Patch
+    label -- the comment content
+    x -- box's x coordinate in the patch (optional)
+    y -- box's y coordinate in the patch (optional)   
+    """
+
     def __init__(self, patch, label, x=None, y=None):
+        """
+        Creates a comment at x and y position.
+
+        The x and y coordinates are optional. If you don't give one, a
+        free space in the patch will be used.
+
+        Example:
+        p = Patch()
+        comment = Comment(p, 'foo bar baz', 100, 100) 
+        c = Comment(p, ':-)')
+        """                        
         super(Comment, self).__init__(patch, 'text', label, x, y)
 
     def __str__(self):
+        """
+        Shows the comment in a pretty formated ASCII-art way :-).
+
+        Example:
+        comment = Comment(p, 'foo bar baz')
+        print comment
+        \" foo bar baz \"
+        """                        
         return '" %s "' % self.label
 
 # GUIs #########################################################################
